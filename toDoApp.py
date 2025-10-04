2a3597ea7336e25076115b6c9ad983f67a37b6bc
ORBEGOSO <aorbegoso@fit.edu.ph>
2025-10-04 14:39:39 +0800

    NewTdo

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,51 @@
 # toDoApp.py
+import os
+
+def clear_screen():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
 
 tasks=[]
 
 def addtask(task) :
   tasks.append(task)
-  print("task added!")
+  print("Task Added!")
 
 def showTasks( ):
     if len(tasks)==0 :
-      print("no tasks yet")
+      print("No Tasks Present")
     else:
      for i in range (len(tasks)):
       print(i+1,".",tasks[i])
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
-    print("task removed!!")
+    tasks.pop(tasknumber-1) 
+    print("Task Removed!")
 
 def main():
     while True:
-        print("1 Add Task")
+        #TO DO Make Beautiful GUI
+        print("1.Add Task")
         print("2.Show Tasks")
         print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
+        print("4.Exit")
+
+        ch = input("Enter Number Choice(1-4): ")
         if ch=="1":
-            t = input("enter task : ")
+            clear_screen()
+            t = input("Enter Task Name: ")
             addtask(t)
         elif ch=="2":
+            clear_screen()
             showTasks()
         elif ch=="3":
-            n=int(input("enter task no to remove: "))
+            clear_screen()
+            showTasks()
+            n=int(input("Enter Task Number to Remove: "))
             removetask(n)   
         elif ch=="4":
-            break;
+            break
         else:
-            print("wrong choice!!")
+            print("Choice was Invalid Please Try Again")
 main()
